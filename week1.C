2ab16d58c1ebfb15321d9eaff884997540b2531a
Aadi satwik pandey <73301776+Aadisatwikpandey@users.noreply.github.com>
2024-04-17 17:19:34 +0530

    program2

--- week1.C
+++ week1.C
@@ -1,5 +1,22 @@
-#include<stdio.h>
-int main(){
-    printf("Hello, World!");
-    return 0;
+// program-1
+
+// #include<stdio.h>
+// int main(){
+//     printf("Hello, World!");
+//     return 0;
+// }
+
+
+/*Program-2:
+Find the largest number among the three numbers. */
+
+#include <stdio.h>
+int main{
+    int number[];N;
+    printf('Enter number of numbers to Enter: ');
+    scanf("%d",&N);
+    for i in range(0,i){
+        scanf("%d",&number)
+    }
+    return 0
 }

d4b3225d1dd301f705d19a325135841e47c6dd04
Aadi satwik pandey <73301776+Aadisatwikpandey@users.noreply.github.com>
2024-08-15 11:27:59 +0530

    Create arithmetic_op.c
    Doing simple arithmetic operations in C

---
+++ arithmetic_op.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a, b;
+    float c, d;
+    
+    
+    scanf("%d[64]", &a);
+    
+  
+    scanf("%d[64]", &b);
+    
+    
+    scanf("%f[64]", &c);
+    
+    
+    scanf("%f[64]", &d);
+    
+    
+    
+    printf("%d %d \n", a+b, a-b );
+    printf("%3.1f %3.1f", c+d, c-d);
+    
+    return 0;
+}

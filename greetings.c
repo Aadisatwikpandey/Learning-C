91e07fca85077439ea7ac6ba8ea917de5c54c2eb
Aadi satwik pandey <73301776+Aadisatwikpandey@users.noreply.github.com>
2024-08-17 09:44:07 +0530

    Create greetings.c
    This program give greeting to a person

---
+++ greetings.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void hello(){
+  printf("Hello world\n");
+}
+
+//to print good bye function
+
+void byee(){
+  printf("Good bye \n");
+  
+}
+
+int main()
+{
+
+  hello();
+  byee();
+  return 0;
+  
+  
+}
